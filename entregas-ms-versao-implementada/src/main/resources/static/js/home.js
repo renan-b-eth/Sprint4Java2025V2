5568edb8ae27dff3aaef2a413b04303c2e6c22bc
renan-b-eth <renanacademic21@gmail.com>
2025-05-21 18:49:28 -0300

    arrumar

--- entregas-ms-versao-implementada/src/main/resources/static/js/home.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-    container: document.getElementById('lottie-container'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '/animations/home.json'
-});
